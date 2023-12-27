6c965e0d73c43af24d4cb83047eea3c399ec4b16
Justice Isreal <justiceyba@gmail.com>
2023-12-27 15:55:05 -0500

    mongoDB ATLAS connection established successful

--- server/index.js
+++ server/index.js
@@ -1,11 +1,15 @@
 const express = require("express");
 const { graphqlHTTP } = require("express-graphql");
+const colors = require("colors");
+const connectDB = require("./config/db");
 const schema = require("./schema/schema");
 require("dotenv").config();
 const port = process.env.PORT || 5000;
 
 const app = express();
 
+// connect to database
+connectDB();
 app.use(
   "/graphql",
   graphqlHTTP({ schema, graphiql: process.env.NODE_ENV === "development" })
--- server/schema/schema.js
+++ server/schema/schema.js
@@ -19,9 +19,44 @@ const ClientType = new GraphQLObjectType({
   }),
 });
 
+// project Typpe
+const ProjectType = new GraphQLObjectType({
+  name: "Project",
+  fields: () => ({
+    id: { type: GraphQLID },
+    name: { type: GraphQLString },
+    description: { type: GraphQLString },
+    status: { type: GraphQLString },
+    client: {
+      type: ClientType,
+      resolve(parent, args) {
+        return clients.find((client) => client.id === parent.clientId);
+      },
+    },
+  }),
+});
+
 const RootQuery = new GraphQLObjectType({
   name: "RootQueryType",
   fields: {
+    // PROJECTS QUERY
+    allProjects: {
+      type: new GraphQLList(ProjectType),
+      resolve(parent, args) {
+        return projects;
+      },
+    },
+    singleProject: {
+      type: ProjectType,
+      args: {
+        id: { type: GraphQLID },
+      },
+      resolve(parent, args) {
+        return projects.find((project) => project.id === args.id);
+      },
+    },
+
+    // CLIENTS QUERY
     allClients: {
       type: new GraphQLList(ClientType),
       resolve(parent, args) {
