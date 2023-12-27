db33d66dc0bf524574475e49964e6ecaa2fef1f3
Justice Isreal <justiceyba@gmail.com>
2023-12-27 14:05:16 -0500

    graphQL client schema setup

---
+++ server/sampleData.js
@@ -0,0 +1,77 @@
+const projects = [
+  {
+    id: "1",
+    clientId: "1",
+    name: "eCommerce Website",
+    description:
+      "We’re passionate about high-impact marketing. Our integrated approach is designed to provide all the talent and tools needed to develop your strategy, craft your message, find your audience, and analyze your performance. Across healthcare, financial services, and other multi-market businesses, we are proven in delivering targeted marketing solutions that drive results. With a full suite of web, content, media, and analytics solutions, we’re all about tracking ROI. Ready to make an impact? Let’s talk.",
+    status: "In progress",
+  },
+  {
+    id: "2",
+    clientId: "2",
+    name: "Dating App",
+    description:
+      "We’re passionate about high-impact marketing. Our integrated approach is designed to provide all the talent and tools needed to develop your strategy, craft your message, find your audience, and analyze your performance. Across healthcare, financial services, and other multi-market businesses, we are proven in delivering targeted marketing solutions that drive results. With a full suite of web, content, media, and analytics solutions, we’re all about tracking ROI. Ready to make an impact? Let’s talk.",
+    status: "In progress",
+  },
+  {
+    id: "3",
+    clientId: "3",
+    name: "SEO Project",
+    description:
+      "We’re passionate about high-impact marketing. Our integrated approach is designed to provide all the talent and tools needed to develop your strategy, craft your message, find your audience, and analyze your performance. Across healthcare, financial services, and other multi-market businesses, we are proven in delivering targeted marketing solutions that drive results. With a full suite of web, content, media, and analytics solutions, we’re all about tracking ROI. Ready to make an impact? Let’s talk.",
+    status: "Done",
+  },
+  {
+    id: "4",
+    clientId: "4",
+    name: "Design Project",
+    description:
+      "We’re passionate about high-impact marketing. Our integrated approach is designed to provide all the talent and tools needed to develop your strategy, craft your message, find your audience, and analyze your performance. Across healthcare, financial services, and other multi-market businesses, we are proven in delivering targeted marketing solutions that drive results. With a full suite of web, content, media, and analytics solutions, we’re all about tracking ROI. Ready to make an impact? Let’s talk.",
+    status: "Done",
+  },
+  {
+    id: "5",
+    clientId: "4",
+    name: "Cyber Security",
+    description:
+      "We’re passionate about high-impact marketing. Our integrated approach is designed to provide all the talent and tools needed to develop your strategy, craft your message, find your audience, and analyze your performance. Across healthcare, financial services, and other multi-market businesses, we are proven in delivering targeted marketing solutions that drive results. With a full suite of web, content, media, and analytics solutions, we’re all about tracking ROI. Ready to make an impact? Let’s talk.",
+    status: "Done",
+  },
+];
+
+const clients = [
+  {
+    id: "1",
+    name: "Tony Stark",
+    email: "iron@sample.com",
+    phone: "234-456-432",
+  },
+  {
+    id: "2",
+    name: "Stark Lime",
+    email: "Lime@sample.com",
+    phone: "234-456-432",
+  },
+  {
+    id: "3",
+    name: "Peter Clerk",
+    email: "Peter@sample.com",
+    phone: "234-456-432",
+  },
+  {
+    id: "4",
+    name: "Sarah Joe",
+    email: "Sarah@sample.com",
+    phone: "234-456-432",
+  },
+  {
+    id: "5",
+    name: "Kate Ann",
+    email: "Ann@sample.com",
+    phone: "234-456-432",
+  },
+];
+
+module.exports = { projects, clients };
---
+++ server/schema/schema.js
@@ -0,0 +1,29 @@
+const { projects, clients } = require("../sampleData");
+
+const { GraphQLObjectType, GraphQLID, GraphQLString } = require("graphql");
+
+// client Typpe
+const ClientType = new GraphQLObjectType({
+  name: "Client",
+  fields: () => ({
+    id: { type: GraphQLID },
+    name: { type: GraphQLString },
+    email: { type: GraphQLString },
+    phone: { type: GraphQLString },
+  }),
+});
+
+const RootQuery = new GraphQLObjectType({
+  name: "RootQueryType",
+  fields: {
+    clients: {
+      type: ClientType,
+      args: {
+        id: { type: GraphQLID },
+      },
+      resolve(parent, args) {
+        return clients.find((client) => client.id === args.id);
+      },
+    },
+  },
+});
